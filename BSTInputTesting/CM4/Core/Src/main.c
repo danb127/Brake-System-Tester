92659540498d4ca6a0c0cda8d3672453c2fd6a11
DMGDy <dylangarza1909@gmail.com>
2024-11-02 19:46:08 -0400

    [BST/main] to int from long double

--- BSTInputTesting/CM4/Core/Src/main.c
+++ BSTInputTesting/CM4/Core/Src/main.c
@@ -237,10 +237,10 @@ int main(void)
               // Not Using String Potentiometer
               estimated_stroke = estimated_stroke_from_duty_cycles(duty_cycle1, duty_cycle2);
               // duty_cycle1,duty_cycle2,estimated_stroke
-              long double dc1 = (long double)duty_cycle1;
-              long double dc2 = (long double)duty_cycle2;
-              long double strk = (long double)stroke;
-              log_info("%Lf,%Lf,%Lf\r\n",dc1,dc2,strk);
+              int dc1 = (int)duty_cycle1;
+              int dc2 = (int)duty_cycle2;
+              int strk = (int)stroke;
+              log_info("%d,%d,%d\r\n",dc1,dc2,strk);
               result = check_bst_values(0, duty_cycle1, duty_cycle2); // 0 since check_bst_values will use estimated stroke
               // stop running once result is evaluated
               if(result != 0)
@@ -644,7 +644,7 @@ int check_bst_values(float estimated_stroke, float duty_cycle1, float duty_cycle
     int case2 = (fabsf(duty_cycle1 - expected_duty_cycle2) <= DUTY_CYCLE_TOLERANCE &&
              fabsf(duty_cycle2 - expected_duty_cycle1) <= DUTY_CYCLE_TOLERANCE);
 
-    int test_passed = (case1 || case2)? 1: -1;
+    int test_passed = ((case1 == 0)&&(case2 == 0))? 1: -1;
     return test_passed;
 }
 
