442c5d18b337e9a90050d0d67627339e22b335c5
danb127 <danbakerr721@yahoo.com>
2024-11-12 18:27:16 -0500

    [BSTTestRelease] built with directory and new executable name

--- BSTTestRelease/CM4/Core/Src/main.c
+++ BSTTestRelease/CM4/Core/Src/main.c
@@ -218,7 +218,7 @@ int main(void)
       OPENAMP_check_for_message();
 
 
-      // HAL_Delay(10);
+      HAL_Delay(10);
 
       if(start)
       {
@@ -464,7 +464,7 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 255;
+  htim3.Init.Prescaler = 127;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 65535;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
